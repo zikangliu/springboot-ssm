ccb17aebef4a48ac004e02fd88cf665e0a7bfee7
zikangliu <zikangliu0@gmail.com>
2023-07-27 20:26:25 +0800

    SpringBoot整合SSM练习

---
+++ src/main/java/com/lzk/service/impl/BookServiceImpl.java
@@ -0,0 +1,40 @@
+package com.lzk.service.impl;
+
+import com.lzk.controller.Code;
+import com.lzk.dao.BookDao;
+import com.lzk.domain.Book;
+import com.lzk.exception.BusinessException;
+import com.lzk.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+    @Autowired
+    private BookDao bookDao;
+
+    public boolean save(Book book) {
+        return bookDao.save(book) > 0;
+    }
+
+    public boolean update(Book book) {
+        return bookDao.update(book) > 0;
+    }
+
+    public boolean delete(Integer id) {
+        return bookDao.delete(id) > 0;
+    }
+
+    public Book getById(Integer id) {
+        if(id == 1){
+            throw new BusinessException(Code.BUSINESS_ERR,"请不要使用你的技术挑战我的耐性!");
+        }
+        return bookDao.getById(id);
+    }
+
+    public List<Book> getAll() {
+        return bookDao.getAll();
+    }
+}
